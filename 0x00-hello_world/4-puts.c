a76137b598ba98bbc6083d74c04fc3b2ef9aebb2
tinsae-tadese <newmee1523@gmail.com>
2022-06-02 00:36:27 -0700

    Hello World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include<stdio.h>
-int main()
+/**
+ *
+ * main: starting point for the program to run.
+ * retruns - Always 0(success)
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\0");
 	return (0);

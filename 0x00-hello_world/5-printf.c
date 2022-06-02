0751be37da3fc11a765f3b7387d7e2d88868ddcf
tinsae-tadese <newmee1523@gmail.com>
2022-06-02 00:53:13 -0700

    Hello World

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\0");
+	printf("with proper grammar, but the outcome is a piece of art\,\0");
 	return (0);
 }

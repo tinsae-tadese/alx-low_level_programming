651887e5a2a91c27376f145a956622bf2668f7a2
tinsae-tadese <newmee1523@gmail.com>
2022-06-02 00:46:31 -0700

    Hello World

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ *main - Entry point of the program
+ *@void: no argumenet
+ *Return: Always 0(success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\0");
+	return (0);
+}

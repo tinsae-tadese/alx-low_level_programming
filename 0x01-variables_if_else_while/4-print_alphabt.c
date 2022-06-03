029fa74abb8efb9669e5041a4dc4612767818e3f
tinsae-tadese <newmee1523@gmail.com>
2022-06-03 04:57:59 -0700

    C variables, If Else , while and For Loop

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,10 +11,10 @@ int main(void)
 char c = 'c';
 for (c = 'a'; c <= 'z'; c++)
 {
-	if (c != 'q' && c != 'e')
-	{
- 	       putchar(c);
-        }
+if (c != 'q' && c != 'e')
+{
+	putchar(c);
+}
 }
 putchar('\n');
 return (0);

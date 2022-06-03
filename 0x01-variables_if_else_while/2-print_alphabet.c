b2d1a556e17089cb7a2928cabc0aef30f68617a3
tinsae-tadese <newmee1523@gmail.com>
2022-06-03 04:36:15 -0700

    C variables, If Else , while and For Loop

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
+/**
+ *main - Entry point
+ *@void: no argument
+ *Return: Always 0(success)
+ */
+int main(void)
+{
+for(char c = 'a'; c <= 'z'; c++)
+{
+	putchar(c);
+}
+putchar('\n');
+return (0);
+}

aa79ac5e078c566c367bd33266b8db3d6f55eb19
tinsae-tadese <newmee1523@gmail.com>
2022-06-03 03:46:02 -0700

    C variables, If Else , while and For Loop

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,11 +11,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (num > 0)
+	if (n > 0)
 	{
 		printf("is positive");
 	}
-	else if (num == 0)
+	else if (n == 0)
 	{
 		printf("is zero");
 	}

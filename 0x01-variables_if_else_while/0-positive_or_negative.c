a130a8ac5c82e19e53adc90c64c5f37d74f88e93
tinsae-tadese <newmee1523@gmail.com>
2022-06-03 03:52:49 -0700

    C variables, If Else , while and For Loop

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
-#include <stdlib.h>
-#include <time.h>
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
 /**
  *main - Entry point of the program
  *@void: no argument
@@ -7,21 +8,21 @@
  */
 int main(void)
 {
-	int n;
+        int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
-	else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
-	else
-	{
-		printf("%d is negative", n");	
-	}
-	return (0);
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+        if (n > 0)
+        {
+                printf("%d is positive", n);
+        }
+        else if (n == 0)
+        {
+                printf("%d is zero", n);
+        }
+        else
+        {
+                printf("%d is negative", n);
+        }
+        return (0);
 }

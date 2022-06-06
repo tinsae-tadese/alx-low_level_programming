991bc0400daa94dd9a9073050c9658af22f707b3
tinsae-tadese <newmee1523@gmail.com>
2022-06-06 09:45:40 -0700

    variables, if else,while

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,1 +1,10 @@
-#!/bin/bash
+#include<stdio.h>
+/**
+ *main - entry point of a program.
+ *@void : takes no argument
+ *Returns: Always 0(Success) 
+ */
+int main()
+{
+ //My code supposed to be in here
+}

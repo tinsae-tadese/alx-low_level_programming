efb35dbfe3026cb879473b0ab9877969e7b0e687
tinsae-tadese <newmee1523@gmail.com>
2022-06-16 05:12:22 -0700

    Pointers, Arrays and Strings

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 #include "main.h"
 /**
- *@n: a pointer to hold address of passed variable.
+ *reset_to_98 - a functions that resets a value to 98
+ *n: a pointer to hold address of passed variable.
  */
 void reset_to_98(int *n)
 {

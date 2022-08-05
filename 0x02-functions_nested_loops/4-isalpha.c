aef17a04f31fcb3ed77a8688b55e7d04a47177d9
tinsae-tadese <newmee1523@gmail.com>
2022-08-05 08:29:48 -0700

    Functions_nested_loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
+#include "main.h"
 /**
-  *_isalpha - a function that checks if a char is in its lower case.
-  *@int c: takes one char as a parameter.
+  *_isalpha(int c) - a function that checks if a char is in its lower case.
+  *@c: takes one char as a parameter.
   *Return: 1 if a char is in lowercase and 0 otherwise.
   */
 int _isalpha(int c)
